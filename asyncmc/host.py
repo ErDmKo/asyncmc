f6112f39f5184573f19ce615b170c63fcadabb40
James Brown <jbrown@easypost.com>
2016-06-09 11:43:36 -0700

    use getaddrinfo to correctly support both IPv4 and IPv6

--- asyncmc/host.py
+++ asyncmc/host.py
@@ -22,9 +22,11 @@ class Host(object):
         self.deaduntil = 0
 
         if ":" in self.host:
-            parts = self.host.split(":")
+            parts = self.host.rsplit(":", 1)
             self.host = parts[0]
             self.port = int(parts[1])
+        if self.host.startswith('[') and self.host.endswith(']'):
+            self.host = self.host[1:-1]
 
         self.sock = None
 
@@ -32,18 +34,34 @@ class Host(object):
         if self.sock:
             return self
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if hasattr(s, 'settimeout'):
-            s.settimeout(constants.SOCKET_TIMEOUT)
-        try:
-            s.connect((self.host, self.port))
-        except socket.timeout as msg:
-            self.mark_dead('connect: {}'.format(msg))
-            return None
-        except socket.error as msg:
-            if isinstance(msg, tuple):
-                msg = msg[1]
-            self.mark_dead('connect: {}'.format(msg))
+        remaining = constants.SOCKET_TIMEOUT
+        last_error = None
+
+        for family, socktype, proto, _, addr in socket.getaddrinfo(
+            self.host, self.port, socket.AF_UNSPEC, socket.SOCK_STREAM
+        ):
+            if not remaining:
+                self.mark_dead('connect: no time left')
+                return None
+            try:
+                s = socket.socket(family, socktype, proto)
+                s.settimeout(remaining)
+                start = time.time()
+                s.connect(addr)
+                break
+            except socket.timeout as msg:
+                self.mark_dead('connect: {}'.format(msg))
+                return None
+            except socket.error as msg:
+                if isinstance(msg, tuple):
+                    msg = msg[1]
+                last_error = msg
+                s.close()
+                duration = time.time() - start
+                remaining = max(remaining - duration, 0)
+        else:
+            # if we never broke out of the getaddr loop
+            self.mark_dead('connect: {}'.format(last_error))
             return None
         self.sock = s
         self.stream = tornado.iostream.IOStream(s)
