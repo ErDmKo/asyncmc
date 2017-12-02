f0095ed9259ff47b814f4b01edf387e15f58a845
Dawid Dec <dawid@MacBook-Pro-Dawid.local>
2017-12-02 16:05:35 +0100

    stats fix for ubuntu

--- asyncmc/client.py
+++ asyncmc/client.py
@@ -126,7 +126,7 @@ class Client(object):
         result = {}
         while resp != b'END\r\n':
             # Ubuntu response fix
-            resp = resp.replace(" (Ubuntu)", "")
+            resp = resp.replace(" (Ubuntu)", "") if not isinstance(resp, bytes) else resp.replace(b" (Ubuntu)", b"")
             terms = resp.split()
 
             if len(terms) == 2 and terms[0] == b'STAT':
